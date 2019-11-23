0b0e8a2c22b7196d0923cdb79ef21c4275907a35
Jim Zhang <phoinikaswj@outlook.com>
2019-11-24 04:06:59 +0800

    use google sheets as data source

---
+++ js/handle.js
@@ -0,0 +1,44 @@
+// 1. 画议员工作经历的图表
+function handleCareerQueryResponse(response) {
+    // 图的定义，数据，图的选项
+    const chart = new google.visualization.Timeline(document.getElementById('career'));
+    const data = response.getDataTable();
+    const options = {
+        width: "100%"
+    };
+    chart.draw(data, options); // 画图
+}
+// 2. 画议员的google trends
+function handleTrendsQueryResponse(response) {
+    const chart = new google.visualization.LineChart(document.getElementById('trends'));
+    const data = response.getDataTable();
+    const options = {
+        title: 'Google Trends',
+        curveType: 'function',
+        legend: { position: 'bottom' },
+        animation:{
+            duration: 1000,
+            easing: 'out',
+            startup: true
+        }
+    };
+    chart.draw(data, options);
+}
+// 3. 画议员的支持率和认知度
+function handleInvestigateQueryResponse(response) {
+    const chart = new google.charts.Bar(document.getElementById('investigate'));
+    const data = response.getDataTable();
+    const options = {
+        series: {
+            0: { axis: 'Support' },
+            1: { axis: 'Recognition' }
+        },
+        axes: {
+            y: {
+                distance: {label: 'Support'},
+                brightness: {side: 'right', label: 'Recognition'}
+            }
+        }
+    };
+    chart.draw(data, google.charts.Bar.convertOptions(options));
+}
